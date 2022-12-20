1715ef2af76bd5c9c748145af97fafd89dbed246
momentchan <mingjyunhung@gmail.com>
2022-12-20 18:30:36 +0800

    remove tracking submodule

--- Assets/Scripts/TrackerController.cs
+++ Assets/Scripts/TrackerController.cs
@@ -1,20 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using BezierTools;
-using mj.gist.tracking.bodyPix;
 using UnityEngine;
 
 public class TrackerController : MonoBehaviour
 {
     [SerializeField] Bezier bezier;
     [SerializeField, Range(0,1)] private float rate = 0;
-    [SerializeField] BodyPoseProvider provider;
 
     void Update()
     {
-
-        var kp = provider.GetKeyPoint(KeypointID.LeftWrist);
-        Debug.Log($"{kp.Score} {kp.Position}");
     }
     private void OnDrawGizmos()
     {
