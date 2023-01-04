a6c854038ae9a2d79608f998b0b8a4065c637456
momentchan <mingjyunhung@gmail.com>
2023-01-04 12:53:16 +0800

    implement spout sender

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -11,7 +11,6 @@ namespace TLF {
         [SerializeField] private CameraMode mode;
         [SerializeField] private Camera sceneCamera;
         [SerializeField] private Camera simulateCamera;
-        [SerializeField] private Camera outputCamera;
          private CameraMode preMode;
 
         public PlayMode Mode => playMode;
@@ -46,26 +45,15 @@ namespace TLF {
                 case CameraMode.Scene:
                     sceneCamera.enabled = true;
                     simulateCamera.enabled = false;
-                    outputCamera.enabled = false;
                     break;
                 case CameraMode.Simulation:
                     sceneCamera.enabled = false;
                     simulateCamera.enabled = true;
-                    outputCamera.enabled = false;
-                    break;
-                case CameraMode.Output:
-                    sceneCamera.enabled = false;
-                    simulateCamera.enabled = false;
-                    outputCamera.enabled = true;
-                    Screen.SetResolution(4096, 1792, false);
                     break;
             }
 
             if (preMode != mode)
             {
-#if UNITY_EDITOR
-                GameViewUtility.TrySetSize(mode == CameraMode.Output ? "TLF Out" : "TLF Scene");
-#endif
                 preMode = mode;
             }
         }
@@ -73,7 +61,7 @@ namespace TLF {
 
         public enum CameraMode
         {
-            Scene, Simulation, Output
+            Scene, Simulation
         }
         public enum PlayMode
         {
